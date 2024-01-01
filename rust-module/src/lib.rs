9f6ddbeff0ae8202d0b3c3ec32e076325e33733f
Biandratti <maxibiandra@gmail.com>
2024-01-01 19:47:03 +0100

    rust format fixed

--- rust-module/src/lib.rs
+++ rust-module/src/lib.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
-use std::os::raw::c_char;
 use std::future::Future;
+use std::os::raw::c_char;
 use std::pin::Pin;
 use tokio::runtime::Runtime;
 
@@ -8,7 +8,6 @@ use tokio::runtime::Runtime;
 pub extern "C" fn multiply_by_two(num: i32) -> i32 {
     num * 2
 }
-
 
 fn httpbin_async() -> Pin<Box<dyn Future<Output = Result<String, reqwest::Error>>>> {
     Box::pin(async {
